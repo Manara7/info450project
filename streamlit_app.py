f3c1c7fc78065a075e5aef143d3c7a1e7e5984d3
Manara7 <89487546+Manara7@users.noreply.github.com>
2026-04-30 18:58:07 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ st.write(
     "This app explores whether a bachelor's degree is enough to secure a substantial salary, "
     "or whether higher income levels are more associated with graduate education."
 )
+ st.write ( "Authors : ␣ Manara, ␣ Feaven , ␣ Caitlin ,  ␣ Justing" )
 
 df = pd.read_csv("streamlit_data.csv")
 df = df[df["INCWAGE"] > 0]
